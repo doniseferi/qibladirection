4a34b0bd874d7330eed9c35c5b11c90e9fb7ab0d
Edonis Seferi <doni.seferi@gmail.com>
2021-05-08 23:18:38 +0100

    version update

---
+++ src/QiblaDirection/Api.fs
@@ -0,0 +1,20 @@
+﻿    module QiblaDirection.Api
+
+[<Struct>]
+type UnvalidatedGeoCoordinates = public {
+    lat: float
+    lon: float
+}
+
+[<Struct>]
+type QiblaDirection = public {
+    trueNorth: float
+}
+
+[<Struct>]
+type ErrorInformation = private {
+    message: string
+    invalidField: string
+}
+
+type QiblaDirectionQueryHandler = UnvalidatedGeoCoordinates -> Result<QiblaDirection, ErrorInformation>
